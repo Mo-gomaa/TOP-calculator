daca9c5f0aa819d0442ac8160ba407d4fb8977bf
Mohamed Gomaa <mo.gomaa.formal@gmail.com>
2024-01-19 16:01:09 +0200

    Finish calculation functionality

--- script.js
+++ script.js
@@ -38,11 +38,13 @@ lightModeToggle.addEventListener("change", () => {
 });
 
 // The calculation unctionality
-let equalClicked = false;
 const input = document.querySelector(".display__input-line");
 const result = document.querySelector(".display__preview-line--result");
 
-const numbersBtns = document.querySelectorAll(".keypad__numbers button");
+const numbersBtns = document.querySelectorAll(
+  ".keypad__numbers button:not(:nth-child(10))"
+);
+const point = document.querySelector(".keypad__numbers button:nth-child(10)");
 const operatorsBtns = document.querySelectorAll(
   ".keypad__operators button:not(:last-child)"
 );
@@ -75,40 +77,65 @@ const additionBtn = document.querySelector(
 const equalBtn = document.querySelector(
   ".keypad__operators button:nth-child(5)"
 );
-// when the user clicks any of the numbers or operators, the clicked button content is concatenated to the input expression
-// and if the user clicks on any of the operators, a space before and after the operators are added aesthsitics
-// if the user clicks on the equal button, the program takes the expression and replaces the ÷ with / and the × with *
-// the program calculates the expression and assign the result to output and then result.textContent = output
+let equalEvaluated;
+function evaluateExpression(expression) {
+  return new Function("return " + expression)();
+}
+
+function calculate() {
+  result.textContent = evaluateExpression(
+    input.textContent.replace(/÷/g, "/").replace(/×/g, "*")
+  );
+  clearInput();
+}
+
+function clearInput() {
+  input.textContent = "";
+}
+
+function clearOutput() {
+  result.textContent = "";
+}
 
 numbersBtns.forEach((El) => {
-  El.addEventListener("click", () => {
-    input.textContent += El.textContent;
-  });
+  El.addEventListener("click", () => (input.textContent += El.textContent));
+});
+
+AC.addEventListener("click", () => {
+  clearInput();
+});
+plusInMinus.addEventListener("click", () => {
+  result.textContent *= 2;
+  plusInMinus.disabled = true;
+});
+perCent.addEventListener("click", () => {
+  result.textContent /= 100;
+  perCent.disabled = true;
+});
+
+equalBtn.addEventListener("click", () => calculate());
+
+point.addEventListener("click", () => {
+  if (input.textContent === "") {
+    input.textContent += "0.";
+  } else {
+    for (term in input.textContent.split(" ")) {
+      if (term.includes(".")) {
+        return;
+      } else {
+        input.textContent += ".";
+      }
+    }
+  }
 });
 
 operatorsBtns.forEach((El) => {
   El.addEventListener("click", () => {
-    input.textContent += ` ${El.textContent} `;
+    if (input.textContent === "") {
+      input.textContent = result.textContent;
+      input.textContent += ` ${El.textContent} `;
+    } else {
+      input.textContent += ` ${El.textContent} `;
+    }
   });
 });
-
-specialOperationsBtns.forEach((El) => {
-  if (equalClicked == true) {
-    El.addEventListener("click", function () {
-      this.disabled = true;
-    });
-  }
-});
-AC.addEventListener("click", () => (input.textContent = ""));
-plusInMinus.addEventListener("click", () => (result.textContent *= 2));
-perCent.addEventListener("click", () => (result.textContent /= 100));
-equalBtn.addEventListener("click", () => {
-  equalClicked = true;
-  let expression = input.textContent.replace(/÷/g, "/").replace(/×/g, "*");
-  console.log(expression);
-  result.textContent = evaluateExpression(expression);
-});
-
-function evaluateExpression(expression) {
-  return new Function("return " + expression)();
-}
